f97b0a0ca2a81e94570636ed5dabc1aa1df6ae4b
bendoujanna <b.vitalina@alustudent.com>
2026-03-02 17:56:25 +0200

    Initial Spring Boot setup with Maven and dependencies

---
+++ src/main/java/com/alu/wellconnect/WellconnectApplication.java
@@ -0,0 +1,13 @@
+package com.alu.wellconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alu/wellconnect/WellconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alu.wellconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
